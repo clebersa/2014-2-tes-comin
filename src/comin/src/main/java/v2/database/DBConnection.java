0ad927a0600559210da68d52603bb271a4112cde
cleber.93cd <cleber.93cd@63410e8b-5520-e398-5c8c-0f3be918b986>
2014-12-03 21:24:56 +0000

    Adicionando pastas do banco de dados.

---
+++ src/comin/src/main/java/v2/database/DBConnection.java
@@ -0,0 +1,91 @@
+package v2.database;
+
+import java.sql.SQLException;
+
+import com.hp.hpl.jena.sdb.SDBFactory;
+import com.hp.hpl.jena.sdb.Store;
+import com.hp.hpl.jena.sdb.StoreDesc;
+import com.hp.hpl.jena.sdb.sql.SDBConnection;
+import com.hp.hpl.jena.sdb.store.DatabaseType;
+import com.hp.hpl.jena.sdb.store.LayoutType;
+import com.hp.hpl.jena.sdb.util.StoreUtils;
+
+/**
+ * Configures and manages the connection to a SGBD.
+ */
+public class DBConnection {
+
+	/**
+	 * URI to access the SGBD containing the JDBC access protocol, the SGBD
+	 * name, IP address of the database server and the communication port.
+	 */
+	private final static String DB_URI = "jdbc:postgresql://localhost:5432/";
+
+	/**
+	 * Name of the database created by the user to store the RDF triples.
+	 */
+	private final static String DB_NAME = "topicos";
+
+	/**
+	 * User name of the database user.
+	 */
+	private final static String DB_USER = "postgres";
+
+	/**
+	 * Password of the database user.
+	 */
+	private final static String DB_PASSWD = "senha123";
+
+	/**
+	 * Reference to the JDBC of the SGBD. <br/>
+	 * The .jar file of the JDBC driver must be included to the project.
+	 */
+	// private final static String DBDRIVER_CLASS = "org.postgresql.Driver";
+
+	/**
+	 * Gets a store
+	 * 
+	 * @return
+	 */
+	public static Store getStore() {
+
+		/*
+		 * StoreDesc permite definir o layout do banco de dados relacional para
+		 * armazenamento de triplas RDF no SGBD em questão.
+		 */
+		StoreDesc storeDesc = new StoreDesc(LayoutType.LayoutTripleNodesIndex,
+				DatabaseType.PostgreSQL);
+
+		/*
+		 * SDBConnection encapsula os parâmetros de conexão ao SGBD e também
+		 * oferece operações de log.
+		 */
+		SDBConnection conn = new SDBConnection(DB_URI + DB_NAME, DB_USER,
+				DB_PASSWD);
+
+		// System.out.println(conn);
+
+		/*
+		 * Store conecta-se ao SGBD segundo o layout e os parâmetros de acesso
+		 * definidos em SDBConnection.
+		 */
+		Store store = SDBFactory.connectStore(conn, storeDesc);
+
+		/*
+		 * Caso o BD (DB_NAME) exista, mas ainda não tenha sido formatado com o
+		 * layout em questão,ele o será e estará pronto para uso!
+		 */
+		try {
+			if (!StoreUtils.isFormatted(store)) {
+				store.getTableFormatter().create();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		// System.out.println(store);
+		return store;
+
+	}
+
+}
---
+++ src/comin/src/main/java/v2/database/DBController.java
@@ -0,0 +1,116 @@
+package v2.database;
+
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.sdb.SDBFactory;
+import com.hp.hpl.jena.sdb.Store;
+
+public class DBController {
+
+	Store store;
+
+	public DBController() {
+		store = DBConnection.getStore();
+	}
+
+	/**
+	 * Stores a model in the database.
+	 * 
+	 * @param model
+	 *            The model to store.
+	 */
+	public void storeModel(Model model) {
+
+		String message = "";
+
+		try {
+			/*
+			 * Criação de modelo RDF conectado a modelo de armazenamento em
+			 * banco de dados relacional.
+			 */
+			Model connectedModel = SDBFactory.connectDefaultModel(this.store);
+
+			/*
+			 * Adiciona as novas TRIPLAS ao banco de dados. Caso alguma tripla
+			 * já exista, ela não é armazenada.
+			 */
+			connectedModel.add(model);
+			message = "Model stored successfully";
+		} catch (Exception e) {
+			e.printStackTrace();
+			message = "Error storing the model. Error: " + e.getMessage();
+		}
+
+		System.out.println(message);
+	}
+
+	/**
+	 * Queries RDF triples in a database.
+	 * 
+	 * @param queryString
+	 *            The string specifying the query.
+	 */
+	public void queryModel(String queryString) {
+
+		/*
+		 * Criação de um objeto Query com a string de consulta na linguagem
+		 * SPARQL.
+		 */
+		Query query = QueryFactory.create(queryString);
+
+		/*
+		 * Conexão ao banco de dados, que retorna uma coleção de GRAFOS
+		 * armazenados.
+		 */
+		Dataset dataset = SDBFactory.connectDataset(store);
+
+		/*
+		 * Interface para a execução de uma única consulta sobre os GRAFOS
+		 * retornados do BD.
+		 */
+		QueryExecution queryExecution = QueryExecutionFactory.create(query,
+				dataset);
+
+		/*
+		 * Executa a consulta definida na string SPARQL e armazena em um
+		 * ResultSet
+		 */
+		ResultSet resultSet = queryExecution.execSelect();
+
+		/*
+		 * Formatador dos resultados de uma consulta.
+		 */
+		ResultSetFormatter.out(resultSet);
+	}
+
+	/**
+	 * Removes all the RDF triples from the database.
+	 */
+	public void removeAll() {
+		String message = "";
+
+		try {
+			/*
+			 * Criação de MODELO RDF conectado a modelo de armazenamento em
+			 * banco de dados relacional.
+			 */
+			Model model = SDBFactory.connectDefaultModel(this.store);
+
+			/*
+			 * Remove todas as TRIPLAS RDF do banco de dados.
+			 */
+			model.removeAll();
+			message = "Database cleaned!";
+		} catch (Exception e) {
+			message = "Error cleaning the database. Error: " + e.getMessage();
+		}
+
+		System.out.println(message);
+	}
+}
